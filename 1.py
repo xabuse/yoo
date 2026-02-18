41efc8aa9cb0cb0b1b2fba79f076dbb25b5e7e54
xabuse <xxxxx.abuse@gmail.com>
2026-02-19 00:43:48 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -113,6 +113,7 @@ create cool stuff. You write the code for it(and for money from it)
 		# [x] implement authorization
 		# [x] connect bootstrap
 		[] project page
+			[] draw, drop a link
 			[] create model with migration, controller for this page (id, user_id, is_done, header, text) "php artisan make:model Project -m"
 			[] /project route
 			[] auth check
