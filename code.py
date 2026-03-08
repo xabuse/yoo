a567cf8e621a0c462757a0f6c3d30015a7b58428
xabuse <xxxxx.abuse@gmail.com>
2026-03-08 12:49:09 +0500

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,5 @@
-
+[] code helper(vue spa + laravel api)
+  [] setup(laravel sail, vue - npm run dev)
+  [] auth
+  [] decomposition page(like in notion or this file: (projects, tabs, checkboxs, if done = gray)
+  [] timer(default timer from ubuntu, track in the end)
