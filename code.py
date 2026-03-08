d59610fdd43ae2df76dbc8de9b755fc852546c2e
xabuse <xxxxx.abuse@gmail.com>
2026-03-08 17:25:31 +0500

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,10 @@
 [] code helper(vue spa + laravel api)
   # [x] setup(cd backend && ./vendor/bin/sail up -d && cd ../frontend && npm run dev)
   # [x] git repo
-  [] token auth 
+  [] token auth (Sanctum)
   [] decomposition page(like in notion or this file: (projects, tabs, checkboxs, if done = gray)
-  [] timer(default timer from ubuntu, track in the end)
-  [] guide to install it in readme
+  [] timer page(default timer from ubuntu, track in the end)
+  [] readme installation guide
+  [] api docs(scramble/?)
+  [] collab with dima
+  [] get feedback(tg chat)
