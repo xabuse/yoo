691beece706c1eaa712b68c0331cd43160574f98
xabuse <xxxxx.abuse@gmail.com>
2026-03-08 14:01:47 +0500

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
 [] code helper(vue spa + laravel api)
-  [] setup(laravel sail, vue - npm run dev)
+  [] setup(cd backend && ./vendor/bin/sail up -d && cd ../frontend && npm run dev)
+  [] git repo
   [] auth
   [] decomposition page(like in notion or this file: (projects, tabs, checkboxs, if done = gray)
   [] timer(default timer from ubuntu, track in the end)
+  [] guide to install it in readme
