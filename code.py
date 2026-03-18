66a1d26473872c6a2f6d5bc3fb72a5d62a9f221f
xabuse <xxxxx.abuse@gmail.com>
2026-03-18 12:53:48 +0500

    Update code.py

--- code.py
+++ code.py
@@ -2,12 +2,12 @@
   # [x] setup(cd backend && ./vendor/bin/sail up -d && cd ../frontend && npm run dev)
   # [x] git repo
   # [x] vue router course https://vueschool.io/courses/vue-router-4-for-everyone - skipped
-  [] token auth (Sanctum)
+  # [x] token auth (Sanctum)
     # [x] crete frontend register and login page
     # [x] create login and register api
       # [x] register api: get name, email, password + return token + create user in db
       # [x] login: get email, password + return token
-    [] connect frontend to api
+    # [x] connect frontend to api
       # [x] frontend validation
         # [x] don't allow empty from frontend(red input and red text under input), add password confirm = password validation
         # [x] bug: why laravel api send me html instead of json? - 'Accept': 'application/json'
@@ -16,10 +16,10 @@
       # [x] save token in localstorage
       # [x] implement redirect
       # [x] frontend redirect if user don't have token in localstorage
-      [] redirect from register and login if user authorized
-      [] if api return 401 - delete token and redirect to login
+      # [x] redirect from register and login if user authorized
   [] decomposition page(like in notion or this file: (projects, tabs, checkboxs, if done = gray)
   [] timer page(default timer from ubuntu, track in the end)
+  [] if api return 401 - delete token and redirect to login
   [] readme installation guide
   [] api docs(scramble/?)
   [] collab with dima
